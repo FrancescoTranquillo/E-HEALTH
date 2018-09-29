cc5c38ddcd889c56fb3dacd08cb8fac87ad67fdf
Francesco Tranquillo <francesco.tranquillo@gmail.com>
2018-09-29 14:21:24 +0200

    Indicizzazione
    Aggiunta indicizzazione nel file che estrae nomi, id e url creando la tabella in csv.
    
    il csv salvato non è quello definitivo. Mancano tutte le altre lettere e tutte le altre pagine

--- R script/getinfo.r
+++ R script/getinfo.r
@@ -1,3 +1,4 @@
+# 1: Caricamento librerie ####
 # General-purpose data wrangling
 library(tidyverse)  
 
@@ -13,31 +14,31 @@ library(rebus)
 #URL parser
 library(urltools)
 
-url<-"https://itunes.apple.com/us/genre/ios-medical/id6020?mt=8&letter=N&page=5#page"
+#2: Lettura URL ####
+
+url<-"https://itunes.apple.com/us/genre/ios-medical/id6020?mt=8&letter=A&page=1#page"
 
 page<-read_html(url)
 
+#3: Estrazione url delle app ####
 urls<- page%>%
   rvest::html_nodes("#selectedcontent li")%>%
   rvest::html_nodes("a")%>%
   rvest::html_attr("href")
 
-
+#4: Estrazione nomi delle app ####
 nomi<- page%>%
   rvest::html_nodes("#selectedcontent li")%>%
   rvest::html_text()
 
-
-pattern<-
-
-
-ids<-gsub("(?<=[id])(\d+)(?=[?mt=8])", "", urls)
-
-str_sub(urls, -50, -60)
-
-df<-data_frame(Name= nomi, URL= urls, ID=ids)
+#5 Estrazione ID delle app ####
+IDs1<-gsub("(?<=)(.*)(id)", "", urls, perl = TRUE)
+IDs2<-gsub("(?<=[id])(\\d+)", "", IDs1, perl = TRUE)
+id<-gsub("\\?mt=8", "", IDs2)
 
 
+#6: Costruzione del dataset ####
+df<-data_frame(Name= nomi, URL= urls, ID=id)
 
+#7: Salvataggio del dataset in formato csv ####
 write.csv(df, "appinfo.csv")
-
