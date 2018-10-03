26635cc6ce4df847da46be305ce263fb6b81c654
fabiocubeddu <43721481+fabiocubeddu@users.noreply.github.com>
2018-10-04 00:40:36 +0200

    ciclo

--- R script/CicloGetappinfo.R
+++ R script/CicloGetappinfo.R
@@ -10,21 +10,15 @@ library(lubridate)
 d = NULL
 df<-read.csv2("app_M_H&F_ENGLISH_ONLY.csv", stringsAsFactors = FALSE)
 dc<- df[,2]
-
-for (i in 100){
-url<- dc[i]
+len<-length(dc)
+cate<-NULL
+ for (i in 1:len){
+url<- paste(dc[i])
 page<-read_html(url)
 
-##diventa numerico il . serve per prendere la riga prima
-pegipattern<-"\\d+"
-
-pegi<-page%>%
-  html_node(".l-row:nth-child(6) .large-6")%>%            ##prendi classe dove trovi età
-  html_text(trim=TRUE)%>%                                 ##pulisco
-  str_extract(., pattern = pegipattern)%>%                ##estrae in . (page)       corrisponde il pattern che ho selezionato sopra, prende cifre
-  as.numeric()
-
-d=rbind(d, data.frame(pdb=pegi))
+category<-page%>%
+  html_node(".large-6 .link")%>%                         
+  html_text(trim=TRUE)
+cate<-c(cate,category)
 }
-
 
