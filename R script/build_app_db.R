85c24fb5be1e53dcc27416a481919a66cc146838
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-10-08 13:05:30 +0200

    Creazione database
    Vengono estratti tutti gli attributi tranne:
    20-21-27.
    
    Da sistemare estrazione della size

--- R script/build_app_db.R
+++ R script/build_app_db.R
@@ -1,69 +1,146 @@
-ibrary(tidyverse)
+rm(list=ls()) #pulisce l'enviroment di tutte le variabili create
+
+# 1 Librerie ####
+library(tidyverse)
 library(rvest)
 library(stringr)
 library(rebus)
-library(urltools)        ##XXX PROBLEMA ENORME
-library(gdata)           ##con queste colonne di getappinfo Error: Columns `average.rating`, `ratings`, `currency` 
-library(anytime)         ##must be length 1 or 4, not 0, 0, 0
+library(urltools)       
+library(gdata)            
+library(anytime)      
 library(dplyr)
 library(lubridate)
 
-avgrating=NULL          ######inizializzo per forza tutto se no il codice mi infama
-ratings=NULL
-pegi=NULL
-category=NULL
-description=NULL
-lastupdate<-NULL
-version<-NULL
-devname<-NULL
-price2<-NULL              ######price2 non chiedermi perch? 2, mi piaceva di pi?
-appid<-NULL
-appname<-NULL
-appurl<-NULL
-lang<-NULL
+# 2 Inizializzazione attributi ####
+avgrating=NULL       #1 
+ratings=NULL         #2
+pegi=NULL            #3
+category=NULL        #4
+description=NULL     #5
+lastupdate<-NULL     #6
+version<-NULL        #7
+devname<-NULL        #8
+price2<-NULL         #9
+appid<-NULL          #10
+appname<-NULL        #11
+appurl<-NULL         #12
+lang<-NULL           #13
+urldev<-NULL         #14
+size<-NULL           #15
+keywords<-NULL       #16
+iddev<-NULL          #17
+releasedate<-NULL    #18
+#19-20 Averageuser rat current + ALL???? in teoria sono due indici
+#20-21 NUm of user rating current+ALL????
+#21 Num user rating  --->gi? fatto?
+fivestar<-NULL       #22 % 5star
+fourstar<-NULL       #23 % 4star
+threestar<-NULL      #24 % 3star (integer)
+twostar<-NULL        #25 % 2star
+onestar<-NULL        #26 % 1star
+#27 Date RETRIEVED  ----->gi? fatto?
 
+#3 Lettura app inglesi ####
 df<-read.csv2("app_M_H&F_ENGLISH_ONLY.csv", stringsAsFactors = FALSE)
 dc<- df[,2]  ##prendo seconda colonna dove ci sono gli url                                                             ##prendo seconda colonna dove ci sono gli url
 d2<- df[,3]
 d3<-df[,1]
 len<-length(dc)                                                          ##prendo lunghezza in len sar? la fine del ciclo quando ci decidiamo con
-##coraggio a mandarlo ed aspettare 6 ore che finisca
 
-for (i in 228:235){                           ###### METTO SOLO 4 PERCH? SE METTESSI "len" (DI RIGA 23) CIAONE, CI METTEREBBE 6 GIORNI E NONCCCCI?VOGLIA
+#4 Estrazione dei 27 attributi ####
+for (i in 288:310){                           ###### METTO SOLO 4 PERCH? SE METTESSI "len" (DI RIGA 23) CIAONE, CI METTEREBBE 6 GIORNI E NONCCCCI?VOGLIA
   url<- dc[i]                             ######url prende l'iesima riga di dc che sarebbe la seconda colonna fatta da getinfo.r dove ci sono gli url e la apro in page
   page<-read_html(url)
   
+
+## Keywords (HO BESTEMMIATO MALE) ####
+  keywords1<-page%>%
+    html_nodes('meta')%>%
+    html_attr('content')
+  keywords1<-keywords1[8]
+  keywords<-c(keywords,keywords1)
   
-  ##LINGUAGGIO ####
- lang1<-page%>%
+## Lingue ####
+  lang1<-page%>%
     html_nodes(".l-row:nth-child(8) .large-6 , .l-row:nth-child(7) .large-6 , .l-row:nth-child(6) .large-6 , .large-6 .we-clamp__contents , .l-row:nth-child(2) .large-6 , .large-6 .link , .information-list__item:nth-child(1) .large-6")%>%
     html_text(trim=TRUE)
   lang1<-lang1[5]
   lang<-c(lang,lang1)
   
-  ##URLDEV
-  URLDEV1<-page%>%
+  
+## Size ####
+  size1<-page%>%
+    html_nodes(".l-row:nth-child(8) .large-6 , .l-row:nth-child(7) .large-6 , .l-row:nth-child(6) .large-6 , .large-6 .we-clamp__contents , .l-row:nth-child(2) .large-6 , .large-6 .link , .information-list__item:nth-child(1) .large-6")%>%
+    html_text(trim=TRUE)
+  size1<-size1[2]
+  size<-c(size,size1)
+  
+  ##PROVA PER GB MB KB CHE NON FUNGE
+  #  patternnum<-"\\d+"
+  # size1<-page%>%
+  # html_nodes(".l-row:nth-child(8) .large-6 , .l-row:nth-child(7) .large-6 , .l-row:nth-child(6) .large-6 , .large-6 .we-clamp__contents , .l-row:nth-child(2) .large-6 , .large-6 .link , .information-list__item:nth-child(1) .large-6")%>%
+  # html_text(trim=TRUE)
+  # size1<-size1[2]
+  # if(grepl("GB",size1)==TRUE){
+  #   num<-str_extract(size1, pattern = patternnum)
+  #   size1<-as.numeric(num)
+  #   size1<-size1[1]*1000
+  # }
+  # if(grepl("KB",size1)==TRUE){
+  #   num<-str_extract(size1, pattern = patternnum)
+  #   size1<-as.numeric(num)
+  #   size1<-size1/1000
+  # }
+  # m<-str_extract(size1, pattern = patternnum)
+  # size1<-as.numeric(num)
+  
+  ## Ratings per star (INTEGER)####
+  ratingsxstar<-page%>%
+    html_nodes(".we-star-bar-graph .we-star-bar-graph__bar__foreground-bar")%>%
+    html_attr("style")%>%
+    str_extract_all(.,"\\d+")%>%
+    as.numeric(.)
+  
+  if(length(ratingsxstar)=="0"){
+    ratingsxstar<-NA
+  }
+  
+  onestar<-c(onestar, last(ratingsxstar))
+  twostar<-c(twostar,ratingsxstar[2])
+  threestar<-c(threestar, ratingsxstar[3])
+  fourstar<-c(fourstar, ratingsxstar[4])
+  fivestar<-c(fivestar,first(ratingsxstar))
+  
+  
+  
+  ##Url dev ####
+  urldev1<-page%>%
     html_node(".app-header__identity")%>%      ##non ? un numero ? una stringa, va beneeeeeee???
     html_nodes(".link")%>%
     html_attr("href")
-  URLDEV<-c(URLDEV,URLDEV1)
-  
-  ##
+  urldev<-c(urldev,urldev1)
   
   
-  ##appid da tabella easy ####
+  ##ID dev ####
+  IDs1<-gsub("(?<=)(.*)(id)", "", urldev1, perl = TRUE)
+  IDs2<-gsub("(?<=[id])(\\d+)", "", IDs1, perl = TRUE)
+  iddev1<-gsub("\\?mt=8", "", IDs2)
+  iddev<-c(iddev,iddev1)
+  
+  
+  ##Appid da tabella easy ####
   appid1<-d2[i]
   appid<-c(appid,appid1)
   
-  ##appname ####
+  ##Appname ####
   appname1<-d3[i]
   appname<-c(appname,appname1)
   
-  #appurl ####
+  #Appurl ####
   appurl1<-dc[i]
   appurl<-c(appurl,appurl1)
   
-  #punteggio medio (DOUBLE)####
+  #Punteggio medio (DOUBLE)####
   avgrating1<-page%>%
     html_nodes("we-clamp__contents")%>%   
     html_text(trim = TRUE)%>%                                  
@@ -74,7 +151,7 @@ for (i in 228:235){                           ###### METTO SOLO 4 PERCH? SE METT
   
   avgrating<-c(avgrating,avgrating1)                           ######allora qui per non sovrascrivere come avevamo fatto in matlab lo richiamo cos?
   
-  #punteggio totale (DOUBLE)####
+  #Punteggio totale (DOUBLE)####
   ratings1<-page%>%
     html_node(".we-customer-ratings__count")%>%
     html_text(trim = TRUE)%>%
@@ -112,14 +189,19 @@ for (i in 228:235){                           ###### METTO SOLO 4 PERCH? SE METT
   description<-c(description,description1)
   
   
-  #Data ultimo update (DATA)####
+  #Data ultimo update e release date (DATA)####
   
   lastupdate1<-page%>%
-    html_node(".version-history__item__release-date")%>%
+    html_nodes(".version-history__item__release-date")%>%
     html_text(trim=TRUE)%>%
-    gsub(",","",.)%>%
-    parse_date_time(., orders = "b d Y", locale="us")        
-  lastupdate<-c(lastupdate,lastupdate1)
+    gsub(",","",.)
+  
+  if(length(lastupdate1)=="0"){
+    lastupdate1<-NA
+  }
+  
+  lastupdate<-c(lastupdate, first(lastupdate1))
+  releasedate<-c(releasedate, last(lastupdate1))
   
   #Ultima versione app (STRINGA) ####
   
@@ -153,5 +235,18 @@ for (i in 228:235){                           ###### METTO SOLO 4 PERCH? SE METT
   
   
 }
-data <- tibble(AppID=appid, Name=appname, AppURL=appurl, description=description, average.rating=avgrating, category=category, pegi=pegi, devname=devname, price=price2, version=version, lastupdate=lastupdate)  ## problemi con queste colonne di getappinfo Error: Columns `average.rating`, `ratings`, `currency` must be length 1 or 4, not 0, 0, 0
-write.csv2(data, "appcategoryratingsPROVAFINALE.csv")
+#5 Creazione database ####
+data <- tibble("App ID"=appid, "App Name"=appname, "App URL"=appurl,
+               "App description"=description, "Keywords"=keywords, "Version"=version,
+               "Age rating"=pegi,"Language(s)"=lang, "Developer ID"=iddev, 
+               "Developer Name"=devname, "Category"=category, "Price"=price,
+               "Size"=size, "Last update date"=anydate(lastupdate),
+               "Release Date"=anydate(releasedate), "Average user ratings-current"=avgrating,
+               "Number of user ratings-current"=ratings, "% of user ratings with 5 stars"=fivestar,
+               "% of user ratings with 4 stars"=fourstar,
+               "% of user ratings with 3 stars"=threestar,
+               "% of user ratings with 2 stars"=twostar,
+               "% of user ratings with 1 star"=onestar)
+
+#6 Scrittura file ####
+write.csv2(data, "App_Database.csv", row.names = FALSE)
