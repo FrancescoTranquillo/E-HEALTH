cddb35d48ca07bb7ad8285edd2644049e3ed22e8
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-10-10 22:00:02 +0200

    Fixato il madonna di getinfo ^^

--- R script/getinfo.r
+++ R script/getinfo.r
@@ -1,89 +1,94 @@
+rm(list = ls())
 # 1: Caricamento librerie ####
-# General-purpose data wrangling
+
 library(tidyverse)
 
-# Parsing of HTML/XML files
 library(rvest)
 
-# String manipulation
 library(stringr)
 
-# Verbose regular expressions
 library(rebus)
 
-#URL parser
 library(urltools)
 
+library(progress)
 #1: Inizializzazione del dataset
-d = NULL
 
 
-#avvio lo script che mi da il max numero
-#di pagine per ogni lettera
-source("maxpagenumber.r")
-maxpagenumber
 
 #2: Cicli for ####
 
 #creazione del vettore delle lettere
-letters<-c(LETTERS,"*")
-lenletters<-length(letters)
+letters <- c(LETTERS, "*")
+lenletters <- length(letters)
+
+filenames <- c("M.csv", "HF.csv")
+
+medical <-
+  "https://itunes.apple.com/us/genre/ios-medical/id6020?mt=8&letter="
+hf <-
+  "https://itunes.apple.com/us/genre/ios-health-fitness/id6013?mt=8&letter="
+
+pagine<- c(medical, hf)
+categories <- c("Medical", "Health&Fitness")
 
 
 
-for(k in 1:2){
-  if (k==1){
-    urlin <- "https://itunes.apple.com/us/genre/ios-health-fitness/id6013?mt=8&letter="
-    cat <- "Health&Fitness"
-  } else {
-    d = NULL
-    urlin <- "https://itunes.apple.com/us/genre/ios-medical/id6020?mt=8&letter="
-    cat <- "Medical"
-    
-  }
-  
+for (k in 1:2) {
+  d = NULL
+  print(k)
+  urlin <- pagine[k]
+  cat <- categories[k]
+  source("maxpagenumber.r")
+  print(maxpagenumber)
   
   #primo for: questo cicla tra tutte le lettere
-  for(i in 1:lenletters) {
+  for (i in 1:lenletters) {
     
+    letter <- letters[i]
     
-    letter<-letters[i]
     #secondo for: questo cicla da 1 al massimo numero
     #di pagine proprio di ogni lettera
-    for(j in 1:2){
-      url<-paste(urlin,letter,"&page=", j,"#page",sep="")
+    for (j in 1:maxpagenumber[i]) {
       
+      urlr <- paste(urlin, letter, "&page=", j, "#page", sep = "")
       
-      page<-read_html(url)
+      print(paste("lettera:", letter, "pagina:", j))
+      
+      page <- read_html(urlr)
       
       #3: Estrazione url delle app ####
-      urls<- page%>%
-        rvest::html_nodes("#selectedcontent li")%>%
-        rvest::html_nodes("a")%>%
+      urls <- page %>%
+        rvest::html_nodes("#selectedcontent li") %>%
+        rvest::html_nodes("a") %>%
         rvest::html_attr("href")
       
       #4: Estrazione nomi delle app ####
-      nomi<- page%>%
-        rvest::html_nodes("#selectedcontent li")%>%
+      nomi <- page %>%
+        rvest::html_nodes("#selectedcontent li") %>%
         rvest::html_text()
       
       #5 Estrazione ID delle app ####
-      IDs1<-gsub("(?<=)(.*)(id)", "", urls, perl = TRUE)
-      IDs2<-gsub("(?<=[id])(\\d+)", "", IDs1, perl = TRUE)
-      id<-gsub("\\?mt=8", "", IDs2)
+      IDs1 <- gsub("(?<=)(.*)(id)", "", urls, perl = TRUE)
+      IDs2 <- gsub("(?<=[id])(\\d+)", "", IDs1, perl = TRUE)
+      id <- gsub("\\?mt=8", "", IDs2)
       
       
       
       #6: Costruzione del dataset ####
-      d = rbind(d, data.frame(Name= nomi, URL= urls, ID=id, Category= cat)) 
-    }
+      d <- rbind(d, data.frame(Name= nomi, URL= urls, ID=id, Category= cat))
+   
+      
+      
+      
+
+    } 
+    
   }
   
-  if (k==1){
-    write.csv2(d, "HF.csv", row.names = FALSE)
-  } else {
-    
-    write.csv2(d, "M.csv", row.names = FALSE)
-  }
+
+  
+  
+  write.csv2(d, filenames[k], row.names = FALSE)
   
 }
