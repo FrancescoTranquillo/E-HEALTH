9788a0aec1918fdb78051c03c443aee13b253f01
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-10-10 10:26:41 +0200

    progress bar!

--- R script/build_app_db.R
+++ R script/build_app_db.R
@@ -5,14 +5,14 @@ library(tidyverse)
 library(rvest)
 library(stringr)
 library(rebus)
-library(urltools)       
-library(gdata)            
-library(anytime)      
+library(urltools)
+library(gdata)
+library(anytime)
 library(dplyr)
 library(lubridate)
-
+library(progress)
 # 2 Inizializzazione attributi ####
-avgrating=NULL       #1 
+avgrating=NULL       #1
 ratings=NULL         #2
 pegi=NULL            #3
 category=NULL        #4
@@ -51,10 +51,12 @@ d3<-df[,1]
 len<-length(dc)                                                          ##prendo lunghezza in len sar? la fine del ciclo quando ci decidiamo con
 
 #4 Estrazione dei 27 attributi ####
+rows<-1000:1120
+p<-progress_estimated(length(rows))
 for (i in 1000:1120){                           ###### METTO SOLO 4 PERCH? SE METTESSI "len" (DI RIGA 23) CIAONE, CI METTEREBBE 6 GIORNI E NONCCCCI?VOGLIA
   url<- dc[i]                             ######url prende l'iesima riga di dc che sarebbe la seconda colonna fatta da getinfo.r dove ci sono gli url e la apro in page
   page<-read_html(url)
-  
+
 
 ## Keywords (HO BESTEMMIATO MALE) ####
   keywords1<-page%>%
@@ -62,22 +64,22 @@ for (i in 1000:1120){                           ###### METTO SOLO 4 PERCH? SE ME
     html_attr('content')
   keywords1<-keywords1[8]
   keywords<-c(keywords,keywords1)
-  
+
 ## Lingue ####
   lang1<-page%>%
     html_nodes(".l-row:nth-child(8) .large-6 , .l-row:nth-child(7) .large-6 , .l-row:nth-child(6) .large-6 , .large-6 .we-clamp__contents , .l-row:nth-child(2) .large-6 , .large-6 .link , .information-list__item:nth-child(1) .large-6")%>%
     html_text(trim=TRUE)
   lang1<-lang1[5]
   lang<-c(lang,lang1)
-  
-  
+
+
 ## Size ####
   patternnum<-"\\sGB|KB|MB"
   size1<-page%>%
     html_nodes(".l-row:nth-child(8) .large-6 , .l-row:nth-child(7) .large-6 , .l-row:nth-child(6) .large-6 , .large-6 .we-clamp__contents , .l-row:nth-child(2) .large-6 , .large-6 .link , .information-list__item:nth-child(1) .large-6")%>%
     html_text(trim=TRUE)
   size1<-size1[2]
-  
+
   num<-gsub(pattern = patternnum, "", size1)
   num<-as.double(num)
   if(grepl("GB",size1)==TRUE){
@@ -88,162 +90,162 @@ for (i in 1000:1120){                           ###### METTO SOLO 4 PERCH? SE ME
     size1<-num
   }
   size<-c(size,size1)
-  
-  
+
+
   ## Ratings per star (INTEGER)####
   ratingsxstar<-page%>%
     html_nodes(".we-star-bar-graph .we-star-bar-graph__bar__foreground-bar")%>%
     html_attr("style")%>%
     str_extract_all(.,"\\d+")%>%
     as.numeric(.)
-  
+
   if(length(ratingsxstar)=="0"){
     ratingsxstar<-NA
   }
-  
+
   onestar<-c(onestar, last(ratingsxstar))
   twostar<-c(twostar,ratingsxstar[2])
   threestar<-c(threestar, ratingsxstar[3])
   fourstar<-c(fourstar, ratingsxstar[4])
   fivestar<-c(fivestar,first(ratingsxstar))
-  
-  
-  
+
+
+
   ##Url dev ####
   urldev1<-page%>%
     html_node(".app-header__identity")%>%      ##non ? un numero ? una stringa, va beneeeeeee???
     html_nodes(".link")%>%
     html_attr("href")
   urldev<-c(urldev,urldev1)
-  
-  
+
+
   ##ID dev ####
   IDs1<-gsub("(?<=)(.*)(id)", "", urldev1, perl = TRUE)
   IDs2<-gsub("(?<=[id])(\\d+)", "", IDs1, perl = TRUE)
   iddev1<-gsub("\\?mt=8", "", IDs2)
   iddev<-c(iddev,iddev1)
-  
-  
+
+
   ##Appid da tabella easy ####
   appid1<-d2[i]
   appid<-c(appid,appid1)
-  
+
   ##Appname ####
   appname1<-d3[i]
   appname<-c(appname,appname1)
-  
+
   #Appurl ####
   appurl1<-dc[i]
   appurl<-c(appurl,appurl1)
-  
+
   #Punteggio medio (DOUBLE)####
   avgrating1<-page%>%
-    html_nodes(".we-customer-ratings__averages__display")%>%   
-    html_text(trim = TRUE)%>%                                  
-    as.numeric(.)                                              
+    html_nodes(".we-customer-ratings__averages__display")%>%
+    html_text(trim = TRUE)%>%
+    as.numeric(.)
   if(length(avgrating1)=="0"){
     avgrating1<-NA
   }
-  
+
   avgrating<-c(avgrating,avgrating1)                           ######allora qui per non sovrascrivere come avevamo fatto in matlab lo richiamo cos?
-  
+
   #Punteggio totale (DOUBLE)####
   ratings1<-page%>%
     html_node(".we-customer-ratings__count")%>%
     html_text(trim = TRUE)%>%
-    gsub(" Ratings", "", .)                                  
+    gsub(" Ratings", "", .)
   ratings1<-as.numeric(sub("K", "e3", ratings1))
   if(length(ratings1)=="0"){
     ratings1<-NA
   }
   ratings<-c(ratings,ratings1)                                ##qui mi infama mi sa il codice dopo da problemi nella costruzione della tabella
-  
+
   #PEGI (DOUBLE)####
-  
+
   pegipattern<-"\\d+"
-  
+
   pegi1<-page%>%
-    html_node(".l-row:nth-child(6) .large-6")%>%            
-    html_text(trim=TRUE)%>%                                 
-    str_extract(., pattern = pegipattern)%>%               
+    html_node(".l-row:nth-child(6) .large-6")%>%
+    html_text(trim=TRUE)%>%
+    str_extract(., pattern = pegipattern)%>%
     as.numeric()
   pegi<-c(pegi,pegi1)
-  
-  
+
+
   # TOLTO QUINDI LASCIO IN COMMENTO Categoria (STRINGA)####
-  
+
   #category1<-page%>%
-   # html_node(".large-6 .link")%>%                         
+   # html_node(".large-6 .link")%>%
     #html_text(trim=TRUE)
   #category<-c(category,category1)
-  
+
   #Descrizione (STRINGA)####
-  
+
   description1<-page%>%
-    html_node(".section__description .we-clamp__contents")%>%      
+    html_node(".section__description .we-clamp__contents")%>%
     html_text(trim=TRUE)
   description<-c(description,description1)
-  
-  
+
+
   #Data ultimo update e release date (DATA)####
-  
+
   lastupdate1<-page%>%
     html_nodes(".version-history__item__release-date")%>%
     html_text(trim=TRUE)%>%
     gsub(",","",.)
-  
+
   if(length(lastupdate1)=="0"){
     lastupdate1<-NA
   }
-  
+
   lastupdate<-c(lastupdate, first(lastupdate1))
   releasedate<-c(releasedate, last(lastupdate1))
-  
+
   #Ultima versione app (STRINGA) ####
-  
+
   version1<-page%>%
-    html_node(".version-history__item__version-number")%>%         ##non ? un numero ? una stringa, va beneeeeeee??? non 
+    html_node(".version-history__item__version-number")%>%         ##non ? un numero ? una stringa, va beneeeeeee??? non
     html_text()
   version<-c(version,version1)
-  
+
   #Developer (STRINGA)####
-  
+
   devname1<-page%>%
-    html_node(".information-list__item:nth-child(1) .large-6")%>%       
+    html_node(".information-list__item:nth-child(1) .large-6")%>%
     html_text(trim=TRUE)
   devname<-c(devname,devname1)
-  
+
   #Prezzo (DOUBLE) e valuta (STRINGA) ####
-  
-  currencypattern<-"^\\W"                                                
-  price1<-page%>%        
+
+  currencypattern<-"^\\W"
+  price1<-page%>%
     html_node(".inline-list__item--bulleted:nth-child(1)")%>%
     html_text(trim=TRUE)
   if(length(price1)=="0"){
     price1<-NA
   }
-  
+
   if(price1=="Free"){
     price1<-0
     currency1<-0                        ##currency=valuta
   } else {
     currency1<-str_extract(price1, currencypattern)
-    price1<-gsub(currencypattern, "", price1)     
+    price1<-gsub(currencypattern, "", price1)
     price1<-as.numeric(price1)
   }
   price<-c(price,price1)
   currency<-c(currency,currency1)
-  
+
 ##DATA
   today1<- Sys.Date()
   today<-c(today,today1)
-  
-    
+
+  print(p$tick())
 }
 #5 Creazione database ####
 data <- tibble("App ID"=appid, "App Name"=appname, "App URL"=appurl,
                "App description"=description, "Keywords"=keywords, "Version"=version,
-               "Age rating"=pegi,"Language(s)"=lang, "Developer ID"=iddev, 
+               "Age rating"=pegi,"Language(s)"=lang, "Developer ID"=iddev,
                "Developer Name"=devname, "Price"=price, "Currency"=currency,
                "Size"=size, "Last update date"=anydate(lastupdate),
                "Release Date"=anydate(releasedate), "Average user ratings-current"=avgrating,
