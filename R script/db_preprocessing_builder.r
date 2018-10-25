7d722befd9cb75be3030cccdfeabffed39f01aad
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-10-26 01:15:05 +0200

    6.3

--- R script/db_preprocessing_builder.r
+++ R script/db_preprocessing_builder.r
@@ -3,13 +3,13 @@ filenames <-
              pattern = "_completed.csv$",
              recursive = T)
 
-my.df <-
-  do.call("rbind",
-          lapply(
-            filenames,
-            read.csv2,
-            header = TRUE,
-            stringsAsFactors = FALSE
-          ))
+df.list <- lapply(filenames,
+                  read.csv2,
+                  header = TRUE,
+                  stringsAsFactors = FALSE)
+
+
+my.df <- bind_rows(df.list)
+
 
 write.csv2(my.df, "Database_preprocessing.csv", row.names = FALSE)
