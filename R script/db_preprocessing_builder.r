897bd5b593289d8d62da063fb207f9e53dc25906
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-10-26 11:49:48 +0200

    Scritto Database preprocessing

--- R script/db_preprocessing_builder.r
+++ R script/db_preprocessing_builder.r
@@ -6,10 +6,13 @@ filenames <-
 df.list <- lapply(filenames,
                   read.csv2,
                   header = TRUE,
-                  stringsAsFactors = FALSE)
+                  stringsAsFactors = FALSE)%>%
+  do.call("rbind",.)
+
+df.list[,16]<-anytime::anydate(df.list[,16])
+df.list[,17]<-anytime::anydate(df.list[,17])
+df.list[,25]<-anytime::anydate(df.list[,25])
 
 
-my.df <- bind_rows(df.list)
 
-
-write.csv2(my.df, "Database_preprocessing.csv", row.names = FALSE)
+write.csv2(df.list, "Database_preprocessing.csv", row.names = FALSE)
