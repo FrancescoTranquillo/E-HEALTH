b2ead9335040fe91ff98ae2c1ed795bbf134ed2d
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-11-19 20:42:34 +0100

    trattamento input e output per metamap

--- R script/meta_input_creator.r
+++ R script/meta_input_creator.r
@@ -1,19 +1,20 @@
 df <-
   read.csv2(
-    "~/Documents/GitHub/E-HEALTH/R script/25_FRA.csv",
+    "25_FRA.csv",
     stringsAsFactors = F,
     header = T
   )
 
-df <- df[,c(1,4)]
+df <- df[,4]
 
 
 write.table(
   df,
-  "~/Documents/GitHub/E-HEALTH/R script/25_FRA.txt",
+  "25_FRA.txt",
   append = F,
   dec = ".",
   col.names = F,
   row.names = F,
-  sep = "\n---\n"
+  #sep = \n",
+  eol = "\n\n"
 )
