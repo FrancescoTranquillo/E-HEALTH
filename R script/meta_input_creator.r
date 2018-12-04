844da1e90bf994f6aecf44b3705b8e492d3584bb
FrancescoTranquillo <37619136+FrancescoTranquillo@users.noreply.github.com>
2018-12-04 20:34:25 +0100

    k

--- R script/meta_input_creator.r
+++ R script/meta_input_creator.r
@@ -1,6 +1,10 @@
 df <-
   read.csv2(
+<<<<<<< HEAD
     "25_FRA.csv",
+=======
+    "Test_metamap.csv",
+>>>>>>> parent of 2c00c88... x
     stringsAsFactors = F,
     header = T
   )
@@ -10,7 +14,11 @@ df <- df[,4]
 
 write.table(
   df,
+<<<<<<< HEAD
   "25_FRA.txt",
+=======
+  "Test_metamap.txt",
+>>>>>>> parent of 2c00c88... x
   append = F,
   dec = ".",
   col.names = F,
