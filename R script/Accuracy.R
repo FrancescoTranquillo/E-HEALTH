91b24e10341e73b91b623b3145998fdd63be3008
Silviatra <43743425+Silviatra@users.noreply.github.com>
2018-12-07 18:08:42 +0100

    Update Accuracy.R

--- R script/Accuracy.R
+++ R script/Accuracy.R
@@ -1,14 +1,23 @@
-df <-read.csv2("Acc_prova.csv"  )
+df <-read.csv2("Acc_prova.csv" , stringsAsFactors = FALSE, header = FALSE)
+df[is.na(df)] <- ""
 count<-0
-df2<- df[,6]
-len<- length(df2)
-df1<- df[,15]
-i<-0
+len<- nrow(df)
+len
+
+i<-7
+
 for (i in 1:len){
-  if (df2[i]==df1[i]) {
+  y <- c(df[i,15], df[i,16], df[i,17])
+  x <- c(df[i,6], df[i,7], df[i,8])
+  k <- intersect(x,y)
+  if (length(k) == 1) {
    count<- count + 1
+  } else if(length(k)==2){
+    count <- count +2
+  } else if(length(k)==3){
+    count <- count + 3
   }
 }
 count
-Accuracy = count/len
+Accuracy = count/(len*3)
 Accuracy
