2d7562091343efa795c9b97ce1f6cdb1bf402f2f
Francesco Tranquillo <francesco.tranquillo@gmail.com>
2018-12-15 10:36:48 +0100

    75 app nostre mandate su metamap

--- R script/meta_input_creator.r
+++ R script/meta_input_creator.r
@@ -1,18 +1,18 @@
 df <-
   read.csv2(
-    "database_preprocessed_english_nc_1_across.csv",
+    "./Tabelle to be consegnate/75_NC_across.csv",
     stringsAsFactors = F,
     header = T
   )
 
-df_across_0<- subset(df, df$Across==0)
+df_across_0<- subset(df, df$across_predicted==0)
 
-df_across_0_descriptions <- df_across_0[,5]
+df_across_0_descriptions <- df_across_0[,4]
 
 
 write.table(
   df_across_0_descriptions,
-  "database_preprocessed_english_nc_1_across_0.txt",
+  "./Tabelle to be consegnate/metamap_75.txt",
   append = F,
   dec = ".",
   col.names = F,
