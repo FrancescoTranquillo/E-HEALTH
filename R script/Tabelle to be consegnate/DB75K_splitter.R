0ca10904b2f2d719179f2e2da1586b7a7c388c96
giuliaferrari22 <43743411+giuliaferrari22@users.noreply.github.com>
2018-12-15 12:52:25 +0100

    Split del DBGrande

---
+++ R script/Tabelle to be consegnate/DB75K_splitter.R
@@ -0,0 +1,40 @@
+
+rm(list = ls())
+
+library(base)
+
+df <- read.csv2("database_english_nc1_across.csv", stringsAsFactors = FALSE) %>%
+  .[!duplicated(.), ]
+
+df <- subset(df, Across == "0")
+n <- as.numeric("6")
+
+df.list <-
+  split(df, rep(1:n, length.out = nrow(df), each = ceiling(nrow(df) / n)))
+
+for (i in 1:n) {
+  filename <- paste0(
+    "./File_Meta_75K/75K_Part",
+    i,
+    ".csv"
+  )
+  write.csv2(df.list[[i]], filename, row.names = FALSE)
+  df1 <- read.csv2(filename,stringsAsFactors = F, header = T)
+  df1_description <- df1$Description
+  filename2 <- paste0(
+    "./File_Meta_75K/75KDescription_Part",
+    i,
+    ".txt"
+  )
+  write.table(
+    df1_description,
+    filename2,
+    append = F,
+    dec = ".",
+    col.names = F,
+    row.names = F,
+    eol = "\n\n"
+  )
+}
+  
+
