17cbb1100ec31bc6e4bbd800eea2cf82c499cd70
Silviatra <43743425+Silviatra@users.noreply.github.com>
2018-12-19 18:01:07 +0100

    Update GIGGIplots.r

--- R script/GIGGIplots.r
+++ R script/GIGGIplots.r
@@ -5,7 +5,10 @@ library(Hmisc)
 library(dplyr)
 
 
-df3<-read.csv2("./Tabelle to be consegnate/Results/Dermatology_top60.csv", header=T, stringsAsFactors=F)
+df3<-read.csv2("./Results/Dermatology_top60.csv", header=T, stringsAsFactors=F)
+
+df5<-read.csv2("./Results/db_totale.csv", header=T, stringsAsFactors=F)
+df5$V1<-factor(df5$V1)
 
 ## Rename
 names(df3) <- local({
@@ -66,7 +69,7 @@ df3$`Subcategory Function` <- factor(df3$`Subcategory Function`,
                                                           "calendar and appointment related apps",
                                                           "support",
                                                           "other"))
-
+install.packages("viridis")
 library(viridis)
 df3a<-count(df3, a=df3$`Subcategory Function`,sort = T)
 
@@ -76,8 +79,64 @@ ggplot(df3a ,aes(x=reorder(a, -n),n))+
   coord_flip()+
   geom_text(aes(label=n), hjust=-0.5, color="black", size=5)+
   labs(y = "count", size=2,x = "Subcategory Function")
-  labs(x = "Subcategory Function", size=2)
+
   
+df4<-read.csv2("./Results/Dermatology.csv", header=T, stringsAsFactors=F)
+
+df_price<-tibble("Top60"=df3$Price, "Dermatology"=df4$Price)
+
+library(reshape2)
+
+df3$Top60<-"1"
+df3<-df3[,-c(5,6)]
+df4<-df4[order(-df4$Ranking),] 
+
+df4<-df4[-(1:60),]
+df4$Top60<-"0"
+dfplot<-rbind(df3, df4)
+dfplot$Category<-factor(dfplot$Category)
+dfplot.m<-melt(dfplot, id.vars = c('Top60','Category'), measure.vars = c('Size'))
+dfplot.m
+p<-ggplot(subset(dfplot.m,subset = dfplot.m$value<150))+
+  geom_boxplot(aes(x=Top60, y=value, fill=Category),alpha=0.6)+
+  theme_minimal(base_size = 16)+
+  labs(y = "Size [MB]", size=2,x = "Top Ranked")+
+  scale_fill_manual(values=viridis(3))+
+  ggtitle("Comparison of Size in Dermatology specialty ")+
+  
+  
+p
+
+count(df5,df5$V1,sort = T)%>%na.omit()
+ggplot(count(df5,df5$V1,sort = T)%>%na.omit() )+
+  geom_bar(x=df5$V1,stat = 'identity')
 
 
+table<-count(df5,df5$V1,sort = T)%>%na.omit()
 
+table$`df5$V1`<-as.character(levels(table$`df5$V1`)[table$`df5$V1`])
+table
+
+names(table)<-"Specialty"
+names(table)[2]<-"n"
+
+table <- within(table, 
+                   Position <- factor(Position, 
+                                      levels=names(sort(table(Position), 
+                                                        decreasing=TRUE))))
+
+p2<-ggplot(data=table, aes(x=reorder(Specialty, -n),n)) +
+  geom_bar(stat="identity",fill=viridis(15), color='black',size=1)+
+  # scale_fill_manual(values=viridis(15))+
+  coord_flip()+
+  theme_minimal(base_size = 18)+
+  labs(y = "n", size=6,x = "Specialty")+
+  ggtitle("Final Classification")+
+  theme(legend.position="none")+
+  geom_text(aes(label=n), hjust=-0.5, color="black", size=5)
+p2
+
+
+table
+x=reorder(a, -n),n))+
+  geom_bar(stat="identity",  width=0.7, color="black",fill=viridis(6), size=1)+
